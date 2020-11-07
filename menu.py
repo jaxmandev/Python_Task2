70fd414803a8043978b5cb437ad9e21e14a7388e
Jax <main@Jaxs-MacBook-Pro.local>
2020-11-07 22:38:14 +0000

    Added solutions

---
+++ menu.py
@@ -0,0 +1,15 @@
+# inititate child class
+# insert menu information
+class Menu:
+    def __init__(self, menu):
+        self.menu = {     
+                'Grilled Seabass\n': 15,
+                'Fried Coconut Shrimp\n': 15,
+                'Caramelized Salmon\n': 15,
+                'Jerk Chicken\n': 18,
+                'Tartare de boeuf\n': 18,
+                'Peking Duck\n': 18,
+                'Halloumi salad \n': 12,
+                'Steamead seasoned aubergine \n': 12,
+                'Lentil Dal with grilled peppers \n': 12
+                }
---
+++ waiter.py
@@ -0,0 +1,52 @@
+# import the parent class
+from menu import Menu
+
+# inititate child class
+# import from parent
+class Waiter(Menu):
+        def __init__(self):
+            super().__init__()
+            self.order = [] # variable unique to this child
+            self.bill = []
+
+        # ask customer his desire: 1-see menu, 2-order, 3-review order, and 4-validate order
+        def ordering(self):
+            while True:
+                choice = input("1 --> Show menu\n 2 --> Add to order\n 3 --> Show order\n 4 --> Satisfied with order\n --> ")
+                
+                # print the menu from parent class
+                if choice == 1:
+                    print(self.menu)
+                
+                # add the input to order list while checking it exists
+                # add the item's price to the bill list
+                elif choice == 2:
+                    add_to_menu = input("--> ")
+                    if add_to_menu in self.menu.keys():
+                        self.order.append(add_to_menu)
+                        for i in self.order:
+                            for j in self.menu:
+                                if i == j:
+                                    self.bill.append(self.menu[j])
+                                else:
+                                    continue
+                        bill = sum(bill)
+                    else:
+                        print("Your order is not in menu")
+                
+                # view the items ordered so far
+                elif choice == 3:
+                    print("\n So far you have ordered: ")
+                    print(self.order)
+                
+                # view the order total price
+                elif choice == 4:
+                    print("Thank you for ordering and enjoy the food! ")
+                    print(f"Current order adds up to £ {bill}" )
+
+# create an instance of the Waiter class
+test1 = Waiter()
+
+# start the ordering function which will ask user for input 
+print(test1.ordering())
+
